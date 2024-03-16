6ac99a43ba86ec0560df17815e1c1b131e6b3353
danh-tyler <151315702+danh-tyler@users.noreply.github.com>
2024-03-15 18:14:17 -0700

    Update Main.java

--- CS497-Spring-2024-InterviewSkills/Assignment4/Problem5/Main.java
+++ CS497-Spring-2024-InterviewSkills/Assignment4/Problem5/Main.java
@@ -1,6 +1,13 @@
 import java.util.*;
 class Solution {
-    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+    public static void (String[] args){
+        int[] nums = {1,2,3,5};
+        int[] soln = new int[2];
+        soln = kthSmallestPrimeFraction(nums,3);
+        System.out.print(soln[0]);
+        System.out.print(soln[1]);
+    }
+    public static int[] kthSmallestPrimeFraction(int[] arr, int k) {
         int length = arr.length;
         PriorityQueue<int[]> queue = new PriorityQueue<>();
         for (int i=0;i<length-1;i++){
