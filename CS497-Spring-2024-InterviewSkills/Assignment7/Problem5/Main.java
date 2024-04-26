ccf58b431def101c5db0526740e6986b498a614e
danh-tyler <151315702+danh-tyler@users.noreply.github.com>
2024-04-25 22:06:45 -0700

    Create Main.java

---
+++ CS497-Spring-2024-InterviewSkills/Assignment7/Problem5/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // test case 1
+        int[] test1 = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] test2 = {4,-1,2,1};
+        int[] test3 = {1};
+        System.out.println(maxSubArray(test1));
+        System.out.println(maxSubArray(test2));
+        System.out.println(maxSubArray(test3));
+    }
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currSum=0;
+        for (int n : nums){
+            if (currSum < 0){
+                currSum=0;
+            }
+            currSum+=n;
+            maxSum = Math.max(maxSum,currSum);
+        }
+        return maxSum;
+    }
+}
