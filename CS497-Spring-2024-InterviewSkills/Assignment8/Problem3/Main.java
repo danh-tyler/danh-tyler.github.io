3f85c4aa33da43c0bddf7422fc88f0d746ec0e05
danh-tyler <151315702+danh-tyler@users.noreply.github.com>
2024-05-02 21:17:01 -0700

    Create Main.java

---
+++ CS497-Spring-2024-InterviewSkills/Assignment8/Problem3/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        // test case 1
+        int[] coins1 = {1,2,5};
+        int amount = 11;
+        System.out.println(coinChange(coins1,amount));
+        // test case 2
+        int[] coins2 = {2};
+        amount = 3;
+        System.out.println(coinChange(coins2,amount));
+        // test case 3
+        int[] coins3 = {1};
+        amount = 0;
+        System.out.println(coinChange(coins3,amount));
+    }
+    public static int coinChange(int[] coins, int amount) {
+        if (amount==0){return 0;}
+        if (amount < coins[0]){return -1;}
+        int result=0;
+        Arrays.sort(coins);
+        for(int i=coins.length - 1;i>=0;i--){
+            while(amount >= coins[i]){
+                amount-=coins[i];
+                result++;
+            }
+        }
+        return amount<=0?result:-1;
+    }
+}
